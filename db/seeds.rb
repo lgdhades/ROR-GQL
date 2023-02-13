aa3bbd2c0d2b25d4b5d00b8b2e23f3ada687285e
lgdhades <hades987456@outlook.com>
2023-02-13 07:52:35 -0500

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,18 @@
 require "net/http"
 require 'uri'
-
+require 'json'
 # hardcoded because it won't be changed during this projects life
 uri = URI('https://hacker-news.firebaseio.com/v0/newstories.json')
 ids = JSON.parse(Net::HTTP.get_response(uri).body)
 # puts ids.body if ids.is_a?(Net::HTTPSuccess)
 
-
-puts ids.length
-
+puts ids.count;
 ids.each{
     |id| 
     uri = URI("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
     response = Net::HTTP.get_response(uri).body
 
+    puts id
     if response != nil
         Article.create(JSON.parse(response))
     end
