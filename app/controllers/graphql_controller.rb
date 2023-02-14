9302e641c329fe4e4be7263a1a6912b23fb7f256
lgdhades <hades987456@outlook.com>
2023-02-14 18:44:57 -0500

    crsf issue

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,6 +2,7 @@ class GraphqlController < ApplicationController
     # If accessing from outside this domain, nullify the session
     # This allows for outside API access while preventing CSRF attacks,
     # but you'll have to authenticate your user separately
+    skip_before_action :verify_authenticity_token
     protect_from_forgery with: :null_session
   
     def execute
