97f8a391f2088de0659feaf8381fd82ea18e1315
lgdhades <hades987456@outlook.com>
2023-02-14 19:13:29 -0500

    csrf problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_filter :cor
-    def cor
-      headers["Access-Control-Allow-Origin"]  = "*"
-      headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-      headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-      head(:ok) if request.request_method == "OPTIONS"
-    end
 end
